60bdca8269b9f907beeb4ef3577a6820a4780b69
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2025-05-07 14:49:06 -0600

    Downloaded Calendar/updated chore assigner

--- roomminder/src/app/chore-assigner/chore-assigner.component.ts
+++ roomminder/src/app/chore-assigner/chore-assigner.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chore-assigner',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './chore-assigner.component.css'
 })
 export class ChoreAssignerComponent {
+  constructor(private router: Router) {}
 
+  submitChore() {
+    const choreInput = (document.querySelector('input[placeholder]') as HTMLInputElement).value;
+    const roommate = (document.getElementById('roommate') as HTMLSelectElement).value;
+    const date = (document.getElementById('date') as HTMLInputElement).value;
+    const priority = (document.getElementById('priority-class') as HTMLSelectElement).value;
+
+    const choreData = {
+      chore: choreInput,
+      roommate,
+      date,
+      priority
+    };
+
+    console.log('Chore submitted:', choreData);
+    this.router.navigate(['/dashboard'], {state: {chore: choreData}});
+  }
 }
